955ac6a540393b82bb81738b0194ef9134a78273
Murillo Comino <murillo_comino@hotmail.com>
2019-10-03 14:49:45 -0300

    Section 17 (70) - Adding Events to Elements

--- src/main/section17schedule/ui/ContactForm.java
+++ src/main/section17schedule/ui/ContactForm.java
@@ -1,6 +1,7 @@
 package section17schedule.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ContactForm extends JFrame {
 
@@ -14,5 +15,25 @@ public class ContactForm extends JFrame {
         setContentPane(rootPanel);
         setSize(500,250);
         setVisible(true);
+
+        //Set the interface to the middle of the screen
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2 );
+
+        //Set the close function
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setListeners();
+    }
+
+    private void setListeners() {
+        buttonSave.addActionListener(e -> {
+
+        });
+
+        buttonCancel.addActionListener(e -> {
+            new MainForm();
+            dispose();
+        });
     }
 }
--- src/main/section17schedule/ui/MainForm.java
+++ src/main/section17schedule/ui/MainForm.java
@@ -2,6 +2,8 @@ package section17schedule.ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm extends JFrame {
 
@@ -16,11 +18,25 @@ public class MainForm extends JFrame {
        setSize(500,250);
        setVisible(true);
 
-       //set the interface to the middle of the screen
+       //Set the interface to the middle of the screen
        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
        setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2 );
 
        //Set the close function
        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+       setListeners();
    }
+
+    private void setListeners() {
+        buttonNewContact.addActionListener(e -> {
+            new ContactForm();
+            dispose();
+        });
+
+        buttonRemove.addActionListener(e -> {
+
+        });
+    }
+
 }
